bcc287a94d96f7575b1a0a7acb4d39e207877220
Chiesurin <diego.chiesurin@iisvittorioveneto.it>
2022-04-16 17:59:22 +0200

    Added CLI class

---
+++ src/main/java/it/iisvittorioveneto/lit/CLI.java
@@ -0,0 +1,62 @@
+package it.iisvittorioveneto.lit;
+
+public class CLI {
+
+    public static void main(String[] args) {
+        //system out print menu
+        CLI cli = new CLI();
+        System.out.flush();
+        //input
+        int input = 0;
+        while(input != -1){
+            System.out.println(cli.menu0());
+            //system input read
+            input = Integer.parseInt(System.console().readLine());
+            switch(input){
+                case 1:
+                    System.out.flush();
+                    Application.createWareHouse();
+                    break;
+            }
+        }
+
+    }
+
+
+    private String menu0(){
+        StringBuilder strb = new StringBuilder();
+        //           1234567890123456789012345678901234567890123456789012345
+        strb.append("-------------------------------------------------------\n");
+        strb.append(" 1. Crea un nuovo Magazzino\n");
+        strb.append(" 2. Apri Magazzino\n");
+        strb.append(" 3. Chiudi Magazzino\n");
+        strb.append(" 4. Gestisci Magazzino\n");
+        strb.append("|                                                     |\n");
+        return strb.toString();
+    }
+
+    private String wareHouseMenu(){
+        StringBuilder strb = new StringBuilder();
+        //           1234567890123456789012345678901234567890123456789012345
+        strb.append("-------------------------------------------------------\n");
+        strb.append(" 1. Importa File\n");
+        strb.append(" 2. Salva Versione\n");
+        strb.append(" 3. Ripristina Versione\n");
+        strb.append(" 4. Visualizza Statistiche\n");
+        strb.append(" 5. Visualizza informazioni Magazzino\n");
+        strb.append(" 6. Esporta Magazzino\n");
+        strb.append("|                                                     |\n");
+        return strb.toString();
+    }
+
+    private String statisticsMenu(){
+        StringBuilder strb = new StringBuilder();
+        //           1234567890123456789012345678901234567890123456789012345
+        strb.append("-------------------------------------------------------\n");
+        strb.append(" 1. Visualizza Statistiche Generali\n");
+        strb.append(" 2. Visualizza Collaboratori\n");
+        strb.append(" 3. Visualizza Statisctiche File\n");
+        strb.append("|                                                     |\n");
+        return strb.toString();
+    }
+}
